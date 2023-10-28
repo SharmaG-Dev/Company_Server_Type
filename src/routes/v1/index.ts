8faa43d4f0d6774dc95df61ee4cb30401ac567e6
Abhishek sharma <99015589+SharmaG-Dev@users.noreply.github.com>
2023-10-28 22:08:29 +0530

    Updated Server blank

--- src/routes/v1/index.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import UserRoutes from 'routes/v1/user'
-
-const router = express.Router()
-
-router.use('/user', UserRoutes)
-
-
-export default router
--- src/routes/v1/user.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-
-const router = express.Router()
-
-router.route('/register').get((req, res) => {
-  res.send('user register ')
-})
-
-export default router
